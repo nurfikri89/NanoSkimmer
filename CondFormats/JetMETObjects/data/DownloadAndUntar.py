bf0f43a5f70790bd8e192af54bb890209da6ec2a
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2024-05-02 15:29:01 +0300

    Update list of JECs

--- CondFormats/JetMETObjects/data/DownloadAndUntar.py
+++ CondFormats/JetMETObjects/data/DownloadAndUntar.py
@@ -6,11 +6,13 @@ import urllib.request
 
 baseURL="https://raw.githubusercontent.com/cms-jet/JECDatabase/master/tarballs/"
 jesFilesList=[
-  "Summer22_22Sep2023_V2_MC.tar.gz",
-  "Summer22EE_22Sep2023_V2_MC.tar.gz",
-  "Summer23Prompt23_V1_MC.tar.gz",
-  "Summer23BPixPrompt23_V1_MC.tar.gz",
-  "Summer23BPixPrompt23_RunD_V1_DATA.tar.gz"
+  #"Summer22_22Sep2023_V2_MC.tar.gz",
+  #"Summer22EE_22Sep2023_V2_MC.tar.gz",
+  #"Summer23Prompt23_V1_MC.tar.gz",
+  #"Summer23BPixPrompt23_V1_MC.tar.gz",
+  #"Summer23BPixPrompt23_RunD_V1_DATA.tar.gz",
+  # "Winter23Prompt23_RunA_V1_DATA.tar.gz"
+  # "Winter23Prompt23_V2_MC.tar.gz",
 ]
 
 for jesFile in jesFilesList:
