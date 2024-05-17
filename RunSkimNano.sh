4e0456449069ce46d401d33002a579bcdf846e2f
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2024-05-17 12:06:49 +0300

    Add part for isMC

--- RunSkimNano.sh
+++ RunSkimNano.sh
@@ -4,14 +4,23 @@ date
 echo ""
 
 SAMPLES=(
-JetMET0_Run2024C-PromptReco-v1_NANOAOD
-JetMET1_Run2024C-PromptReco-v1_NANOAOD
+)
+for SAMPLE in ${SAMPLES[@]};
+do
+  python3 -u SkimNano.py --sample $SAMPLE --cpus 4 --nfiles 2 2>&1 | tee logs/LOG_SkimNano_${SAMPLE}.txt
+done
+
+
+SAMPLES=(
+QCD_Flat2022_Run3Winter24JMENano_GTv10
+QCD_Flat2022_Run3Winter24JMENano_GTv9
 )
 
 for SAMPLE in ${SAMPLES[@]};
 do
-  python3 -u SkimNano.py --sample $SAMPLE --cpus 4 --nfiles -1 2>&1 | tee logs/LOG_SkimNano_${SAMPLE}.txt
+  python3 -u SkimNano.py --isMC --sample $SAMPLE --cpus 4 --nfiles -1 2>&1 | tee logs/LOG_SkimNano_${SAMPLE}.txt
 done
+
 
 echo ""
 echo ""
